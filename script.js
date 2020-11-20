e538566262e8cddf44b83cbb68a2c45385892eac
Philip Noebauer <pnoebauer>
2020-11-20 16:58:11 +0800

    functionality added to dynamically load quotes, and for buttonsto load new quotes and post to twitter

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
 
+//API sometimes returns errors; run additional requests
 let iteration = 0;
 
 // Get Quote from API
 async function getQuote() {
 
+    // console.log(iteration, this, newQuoteButton, this===newQuoteButton);
+    //reset iteration to zero after each click
+    if(this===newQuoteButton) {
+        iteration = 0;
+    }
+
+    // do not run more than 15 iterations for the API request in case of an error
     if(iteration>15) return;
     // proxyUrl to avoid cors errors
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -12,7 +25,24 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        
+        //if Author is blank, add 'Unknown'
+        if(data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        //reduce quote size if the quote is long
+        if(data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        }
+        else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        
+        // console.log(data);
         // throw new Error;
     } 
     catch(error) {
@@ -22,5 +52,20 @@ async function getQuote() {
     }
 }
 
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = quoteText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+
+    //open twitterUrl in new tab
+    window.open(twitterUrl, '_blank');
+}
+
 // On Load
 getQuote();
+
+// Load a new quote when the button is clicked
+newQuoteButton.addEventListener('click', getQuote);
+
+// Button to tweet quote
+twitterButton.addEventListener('click', tweetQuote);
